301ea69462855aa890c07e446a8f0de7524484f7
David <david2020martin@gmail.com>
2025-01-09 13:47:57 -0500

    added animation

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -38,57 +38,61 @@ function Intro() {
           <Header />
         </div>
 
-        <section className="flex flex-col xl:flex-row items-center justify-center min-h-screen" id="home">
-          <div className="container flex flex-col xl:flex-row items-center justify-center">
-            {/* Text Section */}
-            <div
-              className="justify-center text-center xl:text-left xl:w-1/2 xl:pl-16 fade-in opacity-0 translate-y-10 transition-opacity duration-700 ease-out"
-            >
-              <p className="text-gray-300 font-serif text-xl md:text-3xl mt-4">
-                Hello, My name is
-              </p>
-              <h1 className="text-white font-bold font-serif text-3xl md:text-9xl mb-8">
-                David Martin.
-              </h1>
-              <p className="text-gray-300 text-lg md:text-2xl px-3">
-                I'm an aspiring Software Engineer passionate about exploring and
-                building with the latest technologies.
-              </p>
+        <section
+  className="flex flex-col xl:flex-row items-center justify-center min-h-screen"
+  id="home"
+>
+  <div className="container flex flex-col xl:flex-row items-center justify-center">
+    {/* Text Section */}
+    <div
+      className="justify-center text-center xl:text-left xl:w-1/2 xl:pl-16 animate-fadeSlideIn"
+    >
+      <p className="text-gray-300 font-serif text-xl md:text-3xl mt-4">
+        Hello, My name is
+      </p>
+      <h1 className="text-white font-bold font-serif text-3xl md:text-9xl mb-8">
+        David Martin.
+      </h1>
+      <p className="text-gray-300 text-lg md:text-2xl px-3">
+        I'm a Software Engineer passionate about exploring and
+        building with the latest technologies.
+      </p>
 
-              {/* Social Links */}
-              <div className="pt-5 text-xl md:text-4xl">
-                <ul className="flex justify-center xl:justify-start space-x-10">
-                  <li className="text-white hover:text-green-600">
-                    <a href="mailto:david2020martin@gmail.com">
-                      <i className="fa-solid fa-envelope"></i>
-                    </a>
-                  </li>
-                  <li className="text-white hover:text-green-600">
-                    <a href="https://github.com/IMARTYI">
-                      <i className="fa-brands fa-github"></i>
-                    </a>
-                  </li>
-                  <li className="text-white hover:text-green-600">
-                    <a href="https://www.linkedin.com/in/david-martin-a65351243/">
-                      <i className="fa-brands fa-linkedin"></i>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
+      {/* Social Links */}
+      <div className="pt-5 text-xl md:text-4xl">
+        <ul className="flex justify-center xl:justify-start space-x-10">
+          <li className="text-white hover:text-green-600">
+            <a href="mailto:david2020martin@gmail.com">
+              <i className="fa-solid fa-envelope"></i>
+            </a>
+          </li>
+          <li className="text-white hover:text-green-600">
+            <a href="https://github.com/IMARTYI">
+              <i className="fa-brands fa-github"></i>
+            </a>
+          </li>
+          <li className="text-white hover:text-green-600">
+            <a href="https://www.linkedin.com/in/david-martin-a65351243/">
+              <i className="fa-brands fa-linkedin"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
 
-            {/* Image Section */}
-            <div
-              className="flex justify-center items-center mt-8 xl:mt-0 xl:w-1/2 fade-in opacity-0 translate-y-10 transition-opacity duration-700 ease-out"
-            >
-              <img
-                className="md:rounded-full object-cover w-2/3 h-1/2 lg:w-2/3 xl:w-2/3 aspect-square rounded-md"
-                src={mainPhoto}
-                alt="A picture of David Martin"
-              />
-            </div>
-          </div>
-        </section>
+    {/* Image Section */}
+    <div
+      className="flex justify-center items-center mt-8 xl:mt-0 xl:w-1/2 animate-fadeSlideIn opacity-0"
+      style={{ animationDelay: "0.5s" }} /* Optional delay for staggered effect */
+    >
+      <img
+        className="md:rounded-full object-cover w-2/3 h-1/2 lg:w-2/3 xl:w-2/3 aspect-square rounded-md"
+        src={mainPhoto}
+        alt="A picture of David Martin"
+      />
+    </div>
+  </div>
+</section>
 
         <section
           className="w-full fade-in opacity-0 translate-y-10 transition-opacity duration-700 ease-out"
