899745cd40ccae0cde6c69fe4d075375ae12e413
David <david2020martin@gmail.com>
2025-01-09 13:50:19 -0500

    fade in animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ export default {
   ],
   theme: {
     extend: {
+      keyframes: {
+        fadeSlideIn: {
+          "0%": { opacity: 0, transform: "translateX(-50%)" },
+          "100%": { opacity: 1, transform: "translateX(0)" },
+        },
+      },
+      animation: {
+        fadeSlideIn: "fadeSlideIn 1s ease-out forwards",
+      },
      fontFamily:{
       'sans': ['Montserrat', 'Arial', 'sans-serif'],
       'Abril Fatface': ['','']
